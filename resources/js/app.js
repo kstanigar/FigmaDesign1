6c5f2e7f642760fea990c76f087299daaf406f98
Keith Stanigar <ktstanigar@hotmail.com>
2020-05-09 20:36:45 +0200

    Didn't finish

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+function init(){
+    const slides = document.querySelectorAll(".slide");
+    const pages = document.querySelectorAll(".page");
+    const backgrounds = [
+        `radial-gradient(#2B3760, #0B1023)`, 
+        `radial-gradient(#4E3022, #161616)`,
+        `radial-gradient(#4E4342, #161616)`
+    ];
+
+    slides.forEach((slide, index) => {
+        slide.addEventListener("click", function(){
+            changeDots(this);
+        });
+    });
+
+    function changeDots(dot){
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
+        dot.classList.add("active");
+    }
+
+
+}
